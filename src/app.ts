6b6db1e6e54c07bc2aa2daf1eeb03cc20480c198
Thierry Fleury <thierry@codinlab.fr>
2019-03-26 20:31:12 +0100

    Add start script

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,7 @@ const app: express.Application = express();
 app.get(`/`, (req, res) => {
     res.send(`Hello world !`);
 });
+
+const port = process.env.PORT || 3000;
   
-app.listen(3000, () => console.log(`Listening on port 3000`)); // eslint-disable-line
+app.listen(port, () => console.log(`Listening on port ${port}`)); // eslint-disable-line
