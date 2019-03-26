263644967858332a850bc7717077574613446f2b
Thierry Fleury <thierry@codinlab.fr>
2019-03-26 21:21:22 +0100

    Add web.config

--- deploy.cmd
+++ deploy.cmd
@@ -105,7 +105,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-:: 4\. Compile TypeScript
+:: 4. Compile TypeScript
 echo Transpiling TypeScript in %DEPLOYMENT_TARGET%...
 call :ExecuteCmd node %DEPLOYMENT_TARGET%\node_modules\typescript\bin\tsc -p "%DEPLOYMENT_TARGET%"
 
