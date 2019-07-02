58e47b34966e367eb0319a085a84a719f46fcf5f
@lolodubois85 <laurent.mci85@orange.fr>
2019-07-02 20:55:17 +0200

    git commit -m "correction app.js, liste des controllers"

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.get(`/`, (req, res) => {
 });
 
 useExpressServer(app, {
-  controllers: [MembersController]
+  controllers: [MembersController,CompaniesController,IndividualsController]
 });
 
 const port = process.env.PORT || 3000;
