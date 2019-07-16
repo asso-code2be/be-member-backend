0b2e988e3d4270d3665198ad74e9bd55fa1c2850
Thierry Fleury <thierry@codinlab.fr>
2019-07-16 21:43:51 +0200

    Remove model import

--- src/controllers/membersController.ts
+++ src/controllers/membersController.ts
@@ -1,6 +1,5 @@
 import {JsonController, Get, Post, Body} from 'routing-controllers';
 import { MemberList } from '../viewModels';
-import {Member} from "../models/Member";
 import { MemberRepository } from '../repositories';
 
 // https://codebrains.io/express-typescript-routing-controllers/
@@ -15,13 +14,5 @@ export class MembersController {
   @Get('/')
   async getMembers(): Promise<MemberList[]> {
     return await this.memberRepository.GetAll();
-  }
-  @Post()
-  async createMembers (@Body() member: Member) {
-    return await Member.query().insert({
-      firstname: member.firstname,
-      lastname: member.lastname,
-      email: member.email,
-    });
   }
 }
