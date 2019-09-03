224ac0f8eaa4219a853f3a7a16843ff5282a87d4
Lénaïc GROLLEAU <lenaic@lenaic.me>
2019-09-03 20:01:17 +0200

    Added member creation

--- src/repositories/memberRepository.ts
+++ src/repositories/memberRepository.ts
@@ -1,21 +1,11 @@
 import { Service } from "typedi";
-
-export interface StubMemberModel {
-    id: string;
-    displayName: string;
-    email: string;
-}
+import {Member} from "../models/Member";
 
 @Service()
 export class MemberRepository {
     constructor() {}
 
-    public GetAll(): Promise<StubMemberModel[]> {
-        const results: StubMemberModel[] = [
-            { id: '123-abc', email: 'john@doe.io', displayName: 'John Doe'},
-            { id: '234-bcd', email: 'jane@doe.io', displayName: 'Jane Doe'}
-        ];
-
-        return Promise.resolve(results);
+    public async getAll(): Promise<Member[]> {
+        return await Member.query();
     }
 } 
